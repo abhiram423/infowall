7da267f7dd403474ff41fda72a68ef570905175c
abhiram423 <abhiram170803@gmail.com>
2026-07-28 16:47:53 +0530

    changes done for UI icons

--- infowall_web/settings.py
+++ infowall_web/settings.py
@@ -84,12 +84,20 @@ WSGI_APPLICATION = 'infowall_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+import dj_database_url
+
+if os.environ.get("RENDER"):
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ["DATABASE_URL"])
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
